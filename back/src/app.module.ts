48f6e45df81c5d39ccf1aaac6c323f893530cc55
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-02-06 19:22:45 +0300

    main

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { envConfigOptions } from './configs/env.config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongoConfigOptions } from './configs/mongo.config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { SmsModule } from './sms/sms.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(envConfigOptions),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: mongoConfigOptions
+    }),
+    AuthModule,
+    UserModule,
+    SmsModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(cookieParser());
+  app.setGlobalPrefix('api')
+  const configService = app.get(ConfigService)
+  const PORT = configService.get('PORT')
+
+  await app.listen(PORT);
+  console.log("Server started on port " + PORT)
 }
 bootstrap();
