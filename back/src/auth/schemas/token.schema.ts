becd3d8de60296994e6571523daa0baddf963d11
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-02-08 22:12:31 +0300

    update token schema

--- back/src/auth/schemas/token.schema.ts
+++ back/src/auth/schemas/token.schema.ts
@@ -1,16 +1,24 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import mongoose, { Types } from "mongoose";
+import { Transform } from "class-transformer";
+import { Types } from "mongoose";
 import { User } from "src/user/schemas/user.schema";
 
-@Schema()
+@Schema({timestamps: true})
 export class Token {
-    @Prop({ type: { type: mongoose.Schema.Types.ObjectId, ref: 'users'}})
+    @Transform(({ value }) => value.toString())
+    _id: string
+  
+    @Prop({ type: Types.ObjectId, ref: 'User'})
     user: User
 
     @Prop({required: true, unique: true})
     token: string
 
-    _id: Types.ObjectId
+    @Prop({ default: Date.now })
+    createdAt!: Date
+
+    @Prop({ default: Date.now })
+    updatedAt!: Date
 }
 
 export const TokenSchema = SchemaFactory.createForClass(Token)
