c58d7e250608aa22e5e90efc21973efbb8b8e504
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-02-19 19:21:31 +0300

    replace css to scss

--- front/src/types/index.d.ts
+++ front/src/types/index.d.ts
@@ -1,6 +1,15 @@
-declare module '*.module.css'
-
 declare module "*.svg" {
     const content: any;
     export default content;
+}
+
+
+declare module "*.png" {
+    const content: any;
+    export default content;
+}
+
+declare module '*.scss' {
+    const content: {[key: string]: any}
+    export = content
 }
