e80bb0ee932dfa2aa5237485e1cf53f044ff1f7b
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-02-19 19:42:02 +0300

    replace import in main files

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { envConfigOptions } from './configs/env.config';
+import { envConfigOptions } from '@/configs/env.config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { mongoConfigOptions } from './configs/mongo.config';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { SmsModule } from './sms/sms.module';
-import { CategoryModule } from './category/category.module';
-import { DishModule } from './dish/dish.module';
-import { FileModule } from './file/file.module';
+import { mongoConfigOptions } from '@/configs/mongo.config';
+import { AuthModule } from '@/auth/auth.module';
+import { UserModule } from '@/user/user.module';
+import { SmsModule } from '@/sms/sms.module';
+import { CategoryModule } from '@/category/category.module';
+import { DishModule } from '@/dish/dish.module';
+import { FileModule } from '@/file/file.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ServeStaticConfigOptions } from './configs/server-static.config';
-import { BasketModule } from './basket/basket.module';
-import { GallaryModule } from './gallary/gallary.module';
-import { PromotionModule } from './promotion/promotion.module';
-import { PromoCodeModule } from './promo-code/promo-code.module';
+import { BasketModule } from '@/basket/basket.module';
+import { GallaryModule } from '@/gallary/gallary.module';
+import { PromotionModule } from '@/promotion/promotion.module';
+import { PromoCodeModule } from '@/promo-code/promo-code.module';
 
 
 @Module({
