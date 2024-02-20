3d3e7f774648f08ec1c544ff4575abd0b81f129d
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-02-20 18:36:27 +0300

    app.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,17 @@
+import { About } from "./components/about/About";
 import { Header } from "./components/header/Header";
+import { OurTeam } from "./components/ourTeam/OurTeam";
+
+import { Specialty } from "./components/specialty/Specialty";
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
+      <About/>
+      <OurTeam/>
+      <About/>
+      <Specialty/>
     </div>
   );
 }
