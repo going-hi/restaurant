a80467a04dc5bf0c19e803975f787731e98d3234
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-02-21 17:00:25 +0300

    type

--- front/src/types/index.d.ts
+++ front/src/types/index.d.ts
@@ -9,6 +9,11 @@ declare module "*.png" {
     export default content;
 }
 
+declare module "*.jpg" {
+    const content: any;
+    export default content;
+}
+
 declare module '*.scss' {
     const content: {[key: string]: any}
     export = content
