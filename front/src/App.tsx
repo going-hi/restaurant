f481b4e38f3ca1a80979951c42a94869b603bb55
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-02-22 22:41:46 +0300

    add Routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,68 @@
+import { Route, Routes } from "react-router-dom";
 import { Footer } from "./components/footer/Footer";
 import { MainPage } from "./page/Main";
 
 const App = () => {
   return (
-    <>
-      <MainPage/>
-     <Footer/>
-    </>
+
+    <Routes>
+      <Route path="/" element={
+        <>
+          <MainPage/>
+          <Footer/>
+        </>
+      }/>
+      <Route path="/about" element={
+        <>
+          <MainPage/>
+          <Footer/>
+        </>
+      }/>
+
+      <Route path="/team" element={
+        <>
+          <MainPage/>
+          <Footer/>
+        </>
+      }/>
+
+      <Route path="/booking" element={
+        <>
+          <MainPage/>
+          <Footer/>
+        </>
+      }/>
+
+      <Route path="/menu" element={
+        <>
+          <MainPage/>
+          <Footer/>
+        </>
+      }/>
+      
+      <Route path="/galerie" element={
+        <>
+          <MainPage/>
+          <Footer/>
+        </>
+      }/>
+
+      <Route path="/events" element={
+        <>
+          <MainPage/>
+          <Footer/>
+        </>
+      }/>
+
+      <Route path="/contect" element={
+        <>
+          <MainPage/>
+          <Footer/>
+        </>
+      }/>
+      
+    </Routes>
+    
       
   );
 }
