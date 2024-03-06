4fe2229fcd5da5459b9c83a0338938add690a3ff
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-06 21:40:00 +0300

    nav component

--- front/src/components/nav/Nav.tsx
+++ front/src/components/nav/Nav.tsx
@@ -3,12 +3,12 @@ import Logo from '../../img/logo.svg'
 
 export const Nav = () => {
     return (
-            <nav className={styles.nav}>
+            <nav className={styles.nav} >
                 <div className={styles.nav__group}>
-                    <a href="#f" className={styles.nav__item}>home</a>
-                    <a href="#f" className={styles.nav__item}>about</a>
-                    <a href="#f" className={styles.nav__item}>team</a>
-                    <a href="#f" className={styles.nav__item}>booking</a>
+                    <a href="/" className={styles.nav__item}>home</a>
+                    <a href="/contact" className={styles.nav__item}>contact</a>
+                    <a href="/menu" className={styles.nav__item}>menu</a>
+                    <a href="/reviews" className={styles.nav__item}>reviews</a>
                 </div>
 
                 <a href="#a" className={styles.nav__logo}>
@@ -16,10 +16,11 @@ export const Nav = () => {
                 </a>
                 
                 <div className={styles.nav__group}>
-                    <a href="#f" className={styles.nav__item}>menu</a>
-                    <a href="#f" className={styles.nav__item}>galerie</a>
-                    <a href="#f" className={styles.nav__item}>events</a>
-                    <a href="#f" className={styles.nav__item}>contact</a>
+                    
+                    <a href="/galerie" className={styles.nav__item}>galerie</a>
+                    <a href="/booking" className={styles.nav__item}>booking</a>
+                    <a href="/cart" className={styles.nav__item}>cart</a>
+                    <a href="/profile" className={styles.nav__item}>profile</a>
                 </div>
             </nav>
     )
